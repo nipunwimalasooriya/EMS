6e9cb0c7b1318bab2e231ce0c4be5a89d22b8323
Nipun Wimalasooriya <40894068+nipunwimalasooriya@users.noreply.github.com>
2024-07-21 01:02:40 +0530

    bugfix

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,21 +1,46 @@
 import './style.css';
+import { useState } from 'react';
+import axio from 'axios';
+
 const Login = () => {
-    return (
-        <div className='d-flex gap-3 justify-content-center  align-item-center vh-100 loginPage'>
-            <div className='p-3 rounded w-25 border loginForm'>
-                <h2>Login Page</h2>
-                <form action="submit">
-                    <div>
+        const [values, setValues] = useState(
+        {
+            email: '',
+            password: ''
+        }
+        );
+        const storeemail = (e) => setValues ({...values, email: e.target.value});
+        const storepassword = (e) => setValues ({...values, password: e.target.value});
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            axio.post('http://localhost:3000/auth/adiminlogin')
+            .then((res) => {
+                console.log(res);
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+        return (
+        <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+            <div className='p-3 rounded 2-25 border loginForm'>
+                <h2 className = 'text-center text-primary'>Login Page</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className ='mb-3'>
                         <label htmlFor="email"><strong>Email:</strong></label>
-                        <input type="email" name="email"  autoComplete = 'off' placeholder = 'Enter email'  
-                        className='form-control rounded-2'/>
+                        <input type="email" name="email" autoComplete='off' placeholder='Enter email' 
+                        onChange = {storeemail} className='form-control rounded-3'/>
                     </div>
-                    <div>
+                    <div className ='mb-2'>
                         <label htmlFor="password"><strong>Password:</strong></label>
-                        <input type="password" name="password"  autoComplete = 'off' placeholder = 'Enter password'  
-                        className='form-control rounded-2'/>
+                        <input type="password" name="password" autoComplete='off' placeholder='Enter password' 
+                        onChange = {storepassword}className='form-control rounded-3'/>
                     </div>
-                    <button type="submit" className='btn btn-success w-100 rounded-2'>Submit</button>
+                    <div className = 'mb-2'>
+                        <input type="checkbox" name="tick" id="tick" className = 'me-1'/>
+                        <label htmlFor="password">Agree with our terms & conditions</label>
+                    </div>
+                    <button type="submit" className='btn btn-primary w-100 rounded-3'>Log in</button>
                 </form>
             </div>
         </div>
