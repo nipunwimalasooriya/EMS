b77dce935859800e766b81e3cc6eeccf3bd90656
Nipun Wimalasooriya <40894068+nipunwimalasooriya@users.noreply.github.com>
2024-07-25 11:55:31 +0530

    added new routes

--- server/Routes/adminRoute.js
+++ server/Routes/adminRoute.js
@@ -11,15 +11,13 @@ router.post("/adminlogin", (req, res) => {
     if (err) return res.json({ loginStatus: false, Error: "Query error" });
     if (result.length > 0) {
       const email = result[0].email;
-      const token = jwt.sign(
-        { role: "admin", email: email },
-        "jwt_sk",
-        { expiresIn: "1d",}
-    );
-    res.cookie('token', token)
-    return res.json({ loginStatus: true});
+      const token = jwt.sign({ role: "admin", email: email }, "jwt_sk", {
+        expiresIn: "1d",
+      });
+      res.cookie("token", token);
+      return res.json({ loginStatus: true });
     } else {
-        return res.json({loginStatus: false, Error: "Wrong email or password"})
+      return res.json({ loginStatus: false, Error: "Wrong email or password" });
     }
   });
 });
